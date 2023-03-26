7bd4c30841a0ae465b38ab445bf84756cc729600
Anton Person <anton.person@gmail.com>
2023-03-26 09:55:22 +0200

    test(version): add integration test for the build scripts that interact with git

--- build/version.ts
+++ build/version.ts
@@ -8,7 +8,7 @@ const gitPath = resolve(".git");
  * Read the current Git branch from the .git/HEAD file.
  * @returns The current Git branch as a string, or "unknown" if the branch cannot be determined.
  */
-async function readGitBranch(): Promise<string> {
+export async function readGitBranch(): Promise<string> {
   const headPath = resolve(gitPath, "HEAD");
   const headContent = await fs.readFile(headPath, "utf8");
   const match = headContent.match(/ref: refs\/heads\/(.+)/);
@@ -21,7 +21,7 @@ async function readGitBranch(): Promise<string> {
  * @param branch - The Git branch to read the commit hash from.
  * @returns The commit hash as a string.
  */
-async function readGitCommit(branch: string): Promise<string> {
+export async function readGitCommit(branch: string): Promise<string> {
   const commitPath = resolve(gitPath, "refs", "heads", branch);
   const commitHash = await fs.readFile(commitPath, "utf8");
 
@@ -33,7 +33,7 @@ async function readGitCommit(branch: string): Promise<string> {
  * @param commitHash - The commit hash to read the timestamp from.
  * @returns The commit timestamp in ISO format as a string.
  */
-function readGitTimestamp(commitHash: string): string {
+export function readGitTimestamp(commitHash: string): string {
   const timestamp = execSync(`git log -1 --format=%cI ${commitHash}`, {
     encoding: "utf8",
   });
@@ -42,26 +42,29 @@ function readGitTimestamp(commitHash: string): string {
 }
 
 /**
- * Generate the version.generated.json file containing the current Git branch, latest commit hash, and timestamp.
+ * Generate the version.generated.json file containing the current Git branch, latest commit hash, commit timestamp and start timestamp.
  */
-(async () => {
-  try {
-    const branch = await readGitBranch();
-    const commitHash = await readGitCommit(branch);
-    const commitTimestamp = readGitTimestamp(commitHash);
+if (require.main === module) {
+  // Only run if this file is called directly, not when its imported in a test
+  (async () => {
+    try {
+      const branch = await readGitBranch();
+      const commitHash = await readGitCommit(branch);
+      const commitTimestamp = readGitTimestamp(commitHash);
 
-    const gitInfo = {
-      branch,
-      commitHash,
-      commitTimestamp: new Date(commitTimestamp).toISOString(),
-      startTimestamp: new Date().toISOString(),
-    };
-    await fs.writeFile(
-      "src/version.generated.json",
-      JSON.stringify(gitInfo),
-      "utf8"
-    );
-  } catch (error) {
-    console.error("Error generating Git info:", error);
-  }
-})();
+      const versionContent = {
+        branch,
+        commitHash,
+        commitTimestamp: new Date(commitTimestamp).toISOString(),
+        startTimestamp: new Date().toISOString(),
+      };
+      await fs.writeFile(
+        "src/version.generated.json",
+        JSON.stringify(versionContent),
+        "utf8"
+      );
+    } catch (error) {
+      console.error("Error generating Git info:", error);
+    }
+  })();
+}
