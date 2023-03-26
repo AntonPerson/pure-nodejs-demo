269d25d6af3a666f1cb80a09a76c7cdecccb3ea5
Anton Person <anton.person@gmail.com>
2023-03-26 10:09:52 +0200

    test(version): add simple unit test for /version endpoint

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -8,4 +8,16 @@ describe("app", () => {
       expect(app.ping().message).toBe("pong!");
     });
   });
+
+  describe("version", () => {
+    it("should return the correct response shape for version", () => {
+      expect(app.version().message).toMatchObject({
+        node: expect.any(String), // 19.6.0
+        branch: expect.any(String), // main
+        commitHash: expect.any(String), // 40 characters commit hash
+        commitTimestamp: expect.any(String), // ISO timestamp of last commit
+        startTimestamp: expect.any(String), // ISO timestamp of server start
+      });
+    });
+  });
 });
